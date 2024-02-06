3c173c01b0a688a0edc7859859733bf0faf56008
Paul LeRoy <Pleroy33@gmail.com>
2024-02-06 09:43:10 -0600

    created home page, and made buttons for Add employee and Create Schedule.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -19,6 +19,7 @@ import InfoPage from '../InfoPage/InfoPage';
 import LandingPage from '../LandingPage/LandingPage';
 import LoginPage from '../LoginPage/LoginPage';
 import RegisterPage from '../RegisterPage/RegisterPage';
+import AddEmployee from '../AddEmployee/AddEmployee';
 
 import './App.css';
 
@@ -59,6 +60,12 @@ function App() {
           >
             <UserPage />
           </ProtectedRoute>
+
+          <ProtectedRoute
+            exact
+            path="/addemployee">
+              <AddEmployee />
+            </ProtectedRoute>
 
           <ProtectedRoute
             // logged in shows InfoPage else shows LoginPage
--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch,} from 'react-redux';
+import { useHistory} from 'react-router-dom/cjs/react-router-dom.min';
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
+  const dispatch =useDispatch();
+  const history = useHistory()
+  const handleAddClick = () => {
+    history.push('/addemployee')
+  }
+
+
   return (
     <div className="container">
-      <h2>Welcome, {user.username}!</h2>
+      <h2>Hello, {user.username}!</h2>
       <p>Your ID is: {user.id}</p>
+      <button className='btn' onClick={handleAddClick}>Add Employee</button>
+      <button className='btn' onClick={() => dispatch({ type: 'CREATE_SCHEDULE'})}>Create Schedule</button>
       <LogOutButton className="btn" />
     </div>
   );
